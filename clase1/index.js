edac133043987b555f60ed37585f9acdf2be0f01
Patricio Vecino <patriciogabrielvecino151004@gmail.com>
2022-11-12 13:01:14 -0300

    inicioJS

---
+++ clase1/index.js
@@ -0,0 +1,46 @@
+// codigo java
+
+// declaracion de variables
+// let nombre = "Patricio";
+//  let apellido = "Vecino";
+
+
+// // // // // muestra cosas en la consola
+//  console.log(nombre);
+//  muestra el tipo
+//  console.log(typeof(nombre));
+
+// // // // // conjunto
+//  console.log("edad", typeof(edad));
+//  console.log("nombre", typeof(nombre));
+//  console.log("apellido", typeof(apellido));
+
+// const NUMERO = 5;  CONSTANTE
+
+// let nombreBonitoGrande;
+
+
+// sumas
+
+// let numero1=5;
+// let numero2=6;
+// let resultado = numero1 + numero2;
+
+// console.log(resultado);
+
+// let saludo = "HOLA";
+// let mundo = "mundo";
+
+// let mensaje = saludo + " " + mundo;
+
+// let mensaje = `${saludo} ${mundo} genial`;
+
+// console.log(mensaje);
+
+
+
+
+let name = prompt("Ingrese su nombre");
+let mensaje = `Hola ${name}`;
+alert(mensaje);
+
